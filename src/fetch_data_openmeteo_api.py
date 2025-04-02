05f8ee2096fd4006b03362d42652860753f440e8
Afro <aphroditifragkiadaki@gmail.com>
2025-04-02 13:38:56 +0300

    Small updates

--- src/fetch_data_openmeteo_api.py
+++ src/fetch_data_openmeteo_api.py
@@ -4,8 +4,7 @@ import pandas as pd
 from retry_requests import retry
 import os
 
-# Ensure the data directory exists
-data_dir = "../data"  # Adjust this based on your folder structure
+data_dir = "../data"  
 os.makedirs(data_dir, exist_ok=True)
 
 # Setup the Open-Meteo API client with cache and retry on error
@@ -13,7 +12,6 @@ cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
 retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 openmeteo = openmeteo_requests.Client(session=retry_session)
 
-# Make sure all required weather variables are listed here
 # The order of variables in hourly or daily is important to assign them correctly below
 url = "https://archive-api.open-meteo.com/v1/archive"
 params = {
